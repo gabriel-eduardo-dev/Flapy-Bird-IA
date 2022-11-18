6948fe9b0bc827ce7d75ed2b40c3845fe2322285
Gabriel160204 <gabriel16020412@gmail.com>
2022-11-17 23:02:07 -0300

    error when git push

--- build/_deps/raylib-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/gab1604/dev/cpp/Flapy-Bird-IA/build/_deps/raylib-src
-# Build directory: /home/gab1604/dev/cpp/Flapy-Bird-IA/build/_deps/raylib-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("raylib")
--- build/_deps/raylib-build/raylib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/gab1604/dev/cpp/Flapy-Bird-IA/build/_deps/raylib-src/src
-# Build directory: /home/gab1604/dev/cpp/Flapy-Bird-IA/build/_deps/raylib-build/raylib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("external/glfw")
